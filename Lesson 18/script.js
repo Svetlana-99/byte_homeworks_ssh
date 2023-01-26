89cfd86570e4b3723a84e380983d59cdd28b8851
Svetlana <svitlanacernovska@gmail.com>
2023-01-26 14:49:22 +0200

    HomeWork 18 changed

--- Lesson 18/script.js
+++ Lesson 18/script.js
@@ -1,8 +1,6 @@
-let showButton = false;
 
 const containerAll = document.getElementById('container-all');
 const postDiv = document.getElementById('post');
-const commentDiv = document.getElementById('comments');
 const commentElem = document.getElementById('comment');
 
 const BASE_URL ="https://jsonplaceholder.typicode.com";
@@ -49,79 +47,82 @@ const get = (url)=>{
     return get(`${ BASE_URL}/comments`);
   };
 
+  const renderCommentsAfterClick = (numberPostId, postContainer) =>{ 
+    getComments()
+      .then((postId) => {
+        postId.forEach(item =>{
+          if(item.postId === numberPostId){
+            renderComments(item, postContainer)
+          }
+      })
+    })
+      .catch((err) => {
+          if (err.status === 404) {
+            alert("Comments not found!");
+          }
+      });
+    }
+
 
   const  clearBox = (elementID) =>
-  {
-      document.getElementById(elementID).innerHTML = "";
-  }
+  {return document.getElementById(elementID).innerHTML = ""}
+  
 
-  const handleRenderComment = (event, btnComments, numberPostId, postContainer)=>{
-    
-    showButton = !showButton;
-    btnComments.innerText = showButton? 'Hidden comments':'Show comments';
-   
-    if (showButton) {
-      getComments(numberPostId, postContainer)
-    .then((postId) => {
-      postId.forEach(item =>{
-        if(item.postId === numberPostId){
-          renderComments(item, postContainer)
-        }
-    })
-  })
-    .catch((err) => {
-        if (err.status === 404) {
-          alert("Comments not found!");
-        }
-    });
-    } else {
-      clearBox("comment");
-    }
-}
+  const handleShowComment = (event)=>{
+    const currentButton = event.currentTarget;
+    commentElem.className = 'comment-elem';
+
+      if (currentButton.hasAttribute('show')){
+        currentButton.innerText = 'Show comments'
+        currentButton.removeAttribute('show');
+        commentElem.classList.toggle("comment-none")
+      }else{
+        currentButton.setAttribute('show', '');
+        currentButton.innerText = 'Hidden comments' 
+      }
+  clearBox("comment");
+};
 
   const renderPost = (post) => {
     
-     const { title, body, id: postId } = post;
+    const { title, body, id: postId } = post;
+
+    const postContainer = document.createElement("div");
+    const titlePostElem = document.createElement("h2");
+    const bodyPostElem = document.createElement("p");
+    const btnComments = document.createElement("button");
+    const postNumber = document.createElement("h2");
+    postNumber.innerText = postId;
     
-     const postContainer = document.createElement("div");
-      
-      const titlePostElem = document.createElement("h2");
-      const bodyPostElem = document.createElement("p");
-      const btnComments = document.createElement("button");
-      const postNumber = document.createElement("h2");
-      postNumber.innerText = postId;
-      
-      postContainer.className = 'container-post';
-      btnComments.className = 'btn-comments';
+    postContainer.className = 'container-post';
+    btnComments.className = 'btn-comments';
 
-      titlePostElem.innerText = title;
-      bodyPostElem.innerText = body;
-      btnComments.innerText = 'Show comments';
+    titlePostElem.innerText = title;
+    bodyPostElem.innerText = body;
+    btnComments.innerText = 'Show comments';
 
-      
-      postContainer.append(postNumber, titlePostElem, bodyPostElem, btnComments);
-      postDiv.append(postContainer); 
-      
-      
-      btnComments.addEventListener('click',
-      ()=>{
-        handleRenderComment(event, btnComments, postId, postContainer)
-      });
-     
+    
+    postContainer.append(postNumber, titlePostElem, bodyPostElem, btnComments);
+    postDiv.append(postContainer); 
+    
+    btnComments.addEventListener('click',
+    ()=>{
+      renderCommentsAfterClick(postId, postContainer); 
+      handleShowComment(event)
+    });
   };
     
    
   const renderComments =(comment, postContainer) =>{
     const {body} = comment;
-    
+  
     const bodyCommentElem = document.createElement("p");
-    commentElem.className = 'comment-elem';
-      
+   
     bodyCommentElem.innerText = body;
     const lineElem = document.createElement("hr");
     
     commentElem.append(bodyCommentElem, lineElem);
-    postContainer.append(commentElem, commentDiv);
+    postContainer.append(commentElem);
   }
     
  
